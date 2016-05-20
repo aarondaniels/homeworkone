0e2a176e29e0368fb7f688736faa3b760b03c115
Aaron <aaronious@gamil.com>
2016-05-20 17:55:39 +0000

    port setting

--- site.rb
+++ site.rb
@@ -1,11 +1,17 @@
 require 'bundler/setup'
 require 'sinatra'
-set :port, 4000
+
+set :port, '4000'
 set :bind, '0.0.0.0'
+
 get '/' do
   erb :index
 end
+
 get'/italian' do
    "Ciao!"
 end
 
+get '/read_more' do
+  erb :read_more
+end
